ebe09279aec795acc96ed416c67b777a4d0398dc
Davko <66025445+Davko-S@users.noreply.github.com>
2021-02-09 22:01:50 +0100

    Code cleanup + comments

--- Assets/Scripts/RoadManager.cs
+++ Assets/Scripts/RoadManager.cs
@@ -12,32 +12,35 @@ public class RoadManager : MonoBehaviour
     private int amOfPrefabs = 2;
     private int startingChildObjects;
 
-    // Start is called before the first frame update
     void Start()
     {
+        // Assign player position to variable
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;     
+        
         for (int i = 0; i < amOfPrefabs; i++)
         {
             SpawnRoad();
         }
 
+        // Get the number of road prefabs at the beginning
         startingChildObjects = transform.childCount;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Spawn one road prefab depending on player position
         if (playerTransform.position.z > (spawnZ - amOfPrefabs * roadLength))
         {
             SpawnRoad();
         }
+        // Destroy first road prefab (FIFO) if there are more than 4 in total  
         int updatedChildObjects = transform.childCount;
         if (updatedChildObjects - startingChildObjects > 2)
         {
             Destroy(GetComponent<Transform>().GetChild(0).gameObject);
         }
     }
-    // Spawns one road prefab and puts the clone inside RoadManager hierarchy object
+    // Spawns one road prefab and puts the clone inside RoadManager as a child object
     void SpawnRoad()
     {
         int prefabIndex = Random.Range(0, roadPrefabs.Length);
