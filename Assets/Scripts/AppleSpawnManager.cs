d8ffc8881e368ddf1582dd32b55e40e464d1f9a3
Davko <66025445+Davko-S@users.noreply.github.com>
2021-02-22 23:30:27 +0100

    Script for generating apples updated

--- Assets/Scripts/AppleSpawnManager.cs
+++ Assets/Scripts/AppleSpawnManager.cs
@@ -8,14 +8,9 @@ public class AppleSpawnManager : MonoBehaviour
     
     private float spawnRangeX = 5.0f;
     private const float spawnPosY = 2.0f;
-    private float spawnPosZ;
-    private float spawnDistanceToPlayer;
     private float playerStartPosZ;
-
-    private float startDelay = 2.0f;
-    private float spawnRate = 2.0f;
-
     private float playerCurrentPosZ;
+
     private GameObject player;
 
 
@@ -32,14 +27,19 @@ public class AppleSpawnManager : MonoBehaviour
     void Update()
     {
         playerCurrentPosZ = player.transform.position.z;
-        InvokeRepeating("SpawnApple", startDelay, spawnRate);
+
+        if (playerCurrentPosZ - playerStartPosZ > 12)
+        {
+            SpawnApple();
+        }
     }
 
     void SpawnApple()    //Randomly generate apple index and spawn position
     {
         int appleIndex = Random.Range(0, applePrefabs.Length);
-        spawnDistanceToPlayer = Random.Range(12, 16);
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), spawnPosY, spawnPosZ);
+        float spawnDistanceZ = Random.Range(12, 16);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), spawnPosY , playerCurrentPosZ + spawnDistanceZ);
         Instantiate(applePrefabs[appleIndex], spawnPos, applePrefabs[appleIndex].transform.rotation);
+        playerStartPosZ = player.transform.position.z;
     }
 }
